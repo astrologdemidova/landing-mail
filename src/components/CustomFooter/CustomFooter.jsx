2a4e1d8f4fc2cdb5c25781b1172414ffa8e1ecd5
astrologdemidova <80847686+astrologdemidova@users.noreply.github.com>
2021-03-28 15:42:34 +0300

    test nodemailer

---
+++ src/components/CustomFooter/CustomFooter.jsx
@@ -0,0 +1,53 @@
+import * as React from "react"
+import { Link } from "gatsby"
+import { FooterStyled, FooterText, FooterLink, FooterSeparite, FooterLinkReact } from "./styles"
+
+export const CustomFooter = () => {
+    return (
+        <FooterStyled>
+            <FooterSeparite />
+            <FooterText>
+                Демидова Яна
+            </FooterText>
+            {/* <FooterText>
+                ОГРНИП 318784700094120
+            </FooterText> */}
+            <FooterText>
+                г. Рязань
+            </FooterText>
+            {/* <FooterText>
+                <FooterLink href='#'>
+                    +7 999 654 77 69
+                </FooterLink>
+            </FooterText> */}
+            <FooterText>
+                <FooterLink href='mailto:astrologdemidova@mail.ru'>
+                    astrologdemidova@mail.ru
+                </FooterLink>
+            </FooterText>
+
+
+
+            <FooterSeparite />
+
+            <FooterText>
+                <FooterLinkReact href='/policy' colorRed>
+                    Политика конфиденциальности
+                </FooterLinkReact>
+            </FooterText>
+            {/* <FooterText>
+                <FooterLink href='#'>
+                    Договор оферты
+                </FooterLink>
+            </FooterText> */}
+
+            <FooterSeparite />
+
+            <FooterText>
+                <FooterLink href='https://webreznov.ru/' target='_blank'>
+                    #webreznov__studio
+                </FooterLink>
+            </FooterText>
+        </FooterStyled>
+    )
+}
